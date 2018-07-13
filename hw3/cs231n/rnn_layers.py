d30a7f7f88eace84b124e69110f888a56c563ea5
andrey <andre_mp@mail.ru>
2018-07-13 17:34:57 +0300

    gan first

--- hw3/cs231n/rnn_layers.py
+++ hw3/cs231n/rnn_layers.py
@@ -34,7 +34,8 @@ def rnn_step_forward(x, prev_h, Wx, Wh, b):
     # hidden state and any values you need for the backward pass in the next_h   #
     # and cache variables respectively.                                          #
     ##############################################################################
-    pass
+    next_h = np.tanh(x.dot(Wx) + prev_h.dot(Wh) + b)
+
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -63,7 +64,8 @@ def rnn_step_backward(dnext_h, cache):
     # HINT: For the tanh function, you can compute the local derivative in terms #
     # of the output value from tanh.                                             #
     ##############################################################################
-    pass
+    dx, dprev_h, dWx, dWh, db = 0,0,0,0,0
+    
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
